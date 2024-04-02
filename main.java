27872df3fdeecd20aa31d8f6160e42a22cb90fdd
serlysonam <47883763+serlysonam@users.noreply.github.com>
2024-04-02 14:48:00 -0500

    Update  to main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Reference variable
+        Box unitBox;
+        // New Box with constructor
+        unitBox = new Box();
+        // Printing reference variable
+        System.out.println(unitBox);
+    }
+}
+
